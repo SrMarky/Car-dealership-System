1ca91e8f0b0cb56c053ac6eacb0a6bd8f2816fd1
SrMarky <77682657+SrMarky@users.noreply.github.com>
2021-04-16 17:05:09 +0200

    Add files via upload
    Car dealership system 1.0.0 by Marky without without graphical interface.
    Coming soon there will be new updates with additions, controls, and a graphical interface from which you can manage everything.

---
+++ manager/FieldManager.java
@@ -0,0 +1,25 @@
+package applications.dealership.manager;
+
+public class FieldManager {
+
+    public void fieldSetting(String car, Field filed) {
+        switch (filed){
+            case DESCRIPTION_CAR:
+                //TODO
+                break;
+            case MODEL_CAR:
+                //TODO MODEL_CAR
+                break;
+            case DATE_CAR:
+                //TODO DATE_CAR
+                break;
+            case COLOR_CAR:
+                //TODO COLOR_CAR
+                break;
+            case SELLER_CAR:
+                //TODO SELLER_CAR
+            default:
+                break;
+        }
+    }
+}
