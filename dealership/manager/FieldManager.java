dd57781581242eb6d4d8d2a7ae6dc85df47dd040
SrMarky <77682657+SrMarky@users.noreply.github.com>
2021-04-19 16:17:51 +0200

    Update FieldManager.java

--- dealership/manager/FieldManager.java
+++ dealership/manager/FieldManager.java
@@ -4,23 +4,17 @@ import dealership.settings.Field;
 
 public class FieldManager {
 
-    public void fieldSetting(String car, Field filed) {
-        //TODO DA FINIRE
+    private void field(String car, Field filed) {
         switch (filed) {
             case DESCRIPTION_CAR:
-                //TODO
                 break;
             case MODEL_CAR:
-                //TODO MODEL_CAR
                 break;
             case DATE_CAR:
-                //TODO DATE_CAR
                 break;
             case COLOR_CAR:
-                //TODO COLOR_CAR
                 break;
             case SELLER_CAR:
-                //TODO SELLER_CAR
             default:
                 break;
         }
